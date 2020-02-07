80bb890f421e360ac51a0f09778030d01f73b55e
JeanFraga <fragajean7@gmail.com>
2020-02-06 19:33:50 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ sudo apt-get update && sudo apt-get upgrade -y
 curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-git clone --single-branch --branch Jean_EC2test https://github.com/JeanFraga/TaggerDocker
+git clone https://github.com/JeanFraga/TaggerDocker
 cd TaggerDocker
 
 sudo docker-compose up --build
