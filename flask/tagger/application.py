d5e95d6174853d0283205e3459dd223ae2ed1b8d
JeanFraga <fragajean7@gmail.com>
2020-02-12 18:08:16 -0500

    added working api to EC2

--- flask/tagger/application.py
+++ flask/tagger/application.py
@@ -1,45 +1,95 @@
 # Imports
 from .classes.new_email_functions import IMap
-from .classes.Basillica import BasilicaAPI
+from .classes.basilica_functions import BasilicaAPI
 from flask import Flask, request, jsonify
 import json
 import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 import pickle
-# from .db import DB, User
+from db import DB, User
 from decouple import config
+from dotenv import load_dotenv
+from io import BytesIO
+from sqlalchemy import exists
 # load_dotenv()
 
 def create_app():
     APP = Flask(__name__)
-    # APP.config["SQLALCHEMY_DATABASE_URI"] = config("DATABASE_URL")
-    # DB.init_app(app)
-    # with app.app_context():
-    #     DB.create_all()
-    # basilica_client = BasilicaAPI()
+    APP.config["SQLALCHEMY_DATABASE_URI"] = config("DATABASE_URL")
+    APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    DB.init_app(APP)
+    with APP.app_context():
+        DB.drop_all()
+        DB.create_all()
+    basilica_client = BasilicaAPI()
 
     @APP.route('/', methods=['GET'])
     def test():
         return "Hello World!"
+
+    # @APP.route('/reset')
+    # def reset():
+    #     DB.drop_all()
+    #     DB.create_all()
+    #     return "reset database"
     
-    # @APP.route('/train_model', methods=["POST"])
-    # def train_model():
-    # j = json.loads(request.data) # Get JSON and convert to DataFrame
-    # df = pd.DataFrame(data=j["emails"])
-    # basilica_client.df = df
+    @APP.route('/train_model', methods=["POST"])
+    def train_model():
+        # Get JSON and convert to DataFrame
+        j = json.loads(request.data)
+        df = pd.DataFrame(data=j["emails"])
 
-    # df = basilica_client.embed_basilica_to_df() # Embed Emails
+        # Embed emails
+        basilica_client.df = df
+        df = basilica_client.embed_basilica_to_df()
 
-    # X = np.vstack(np.array(df['embedded'])) # Train Model
-    # y = df["id_email"].values
-    # knn = KNeighborsClassifier(n_neighbors=5)
-    # knn.fit(X, y)
+        # Train model
+        X = np.vstack(np.array(df['embedded']))
+        y = df["id_email"].values
+        knn = KNeighborsClassifier(n_neighbors=5)
+        knn.fit(X, y)
 
-    # pkl = pickle.dumps(knn) # Save model Pickle into the Database
-    # db_user = User(email_address=j["address"], pickle_file=pkl)
-    # DB.session.add(db_user)
-    # DB.session.commit()
-    # return "Trained a model!"
+        # Save/compress model's pickle into the database
+        file_obj = BytesIO() 
+        joblib.dump(knn, file_obj, compress=3)
+        file_obj.seek(0)
+        pkl = file_obj.getvalue()
+
+        # Check if user already exists
+        db_user = DB.session.query(exists().where(User.email_address == j["address"])).scalar()
+        if db_user: 
+            # Update pickle if user exists
+            db_user.pickle_file = pkl
+        else:
+            # Make new user if one doesn't exist
+            db_user = User(email_address=j["address"], pickle_file=pkl)
+            DB.session.add(db_user)
+        DB.session.commit()
+        file_obj.close()
+        return "Trained a model!"
+
+        @app.route("/predict", methods=["GET"])
+        def predict():
+            # Get JSON and convert to DataFrame
+            j = json.loads(request.data)
+            df = pd.DataFrame(data=j["emails"])
+
+            # Check if user already exists
+            db_user = DB.session.query(exists().where(User.email_address == j["address"])).scalar()
+            if db_user: 
+                # Load pickle and get predictions
+                basilica_client.df = df
+                df = basilica_client.embed_basilica_to_df()
+                file_obj = BytesIO(db_user.pickle_file)
+                pkl = joblib.load(file_obj)
+                res = pkl.kneighbors(np.vstack(np.array(df['embedded'])), n_neighbors=5)[1][0]
+                file_obj.close()
+                return json.dumps({"prediction": [str(i) for i in res]})
+            else:
+                # Return error
+                return "No model in database for {}...".format(j["address"])
+    
+
 
     return APP
