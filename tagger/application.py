d1c017a7b2431bded762b7e103e9889e7ed72d55
JeanFraga <fragajean7@gmail.com>
2020-02-26 18:32:42 -0500

    added heroku app for testing

--- tagger/application.py
+++ tagger/application.py
@@ -26,17 +26,9 @@ def create_app():
     #     DB.drop_all()
     #     DB.create_all()
     basilica_client = BasilicaAPI()
-
-
-    # This lines can be enabled for testing locally but in production will erase the database if the web or anyone has access to /reset.
-    @APP.route('/resetvbvnpjtzmndypmrosuuvqxqugtaburqm')
-    def reset():
-        DB.drop_all()
-        DB.create_all()
-        return "reset database"
     
-    @APP.route('/train_model', methods=["POST"])
-    def train_model():
+    @APP.route("/predict", methods=["POST"])
+    def predict():
         # Get JSON and convert to DataFrame
         data = b""
         while True:
@@ -50,34 +42,20 @@ def create_app():
             return "Could not process input stream."
         df = pd.DataFrame(data=j["emails"])
 
-        # Embed emails
-        basilica_client.df = df
-        df = basilica_client.embed_basilica_to_df()
-
-        # Train model
-        X = np.vstack(np.array(df['embedded']))
-        y = df["id_email"].values
-        knn = KNeighborsClassifier(n_neighbors=5)
-        knn.fit(X, y)
-
-        # Save/compress model's pickle into the database
-        file_obj = BytesIO() 
-        joblib.dump(knn, file_obj, compress=3)
-        file_obj.seek(0)
-        pkl = file_obj.getvalue()
-
         # Check if user already exists
         db_user = User.query.filter(User.email_address == j["address"]).scalar()
         if db_user: 
-            # Update pickle if user exists
-            db_user.pickle_file = pkl
+            # Load pickle and get predictions
+            basilica_client.df = df
+            df = basilica_client.embed_basilica_to_df()
+            file_obj = BytesIO(db_user.pickle_file)
+            pkl = joblib.load(file_obj)
+            res = pkl.kneighbors(np.vstack(np.array(df['embedded'])), n_neighbors=5)[1][0]
+            file_obj.close()
+            return json.dumps({"prediction": [str(i) for i in res]})
         else:
-            # Make new user if one doesn't exist
-            db_user = User(email_address=j["address"], pickle_file=pkl)
-            DB.session.add(db_user)
-        DB.session.commit()
-        file_obj.close()
-        return "Trained a model!"
+            # Return error
+            return "No model in database for {}...".format(j["address"])
 
 
     return APP
